a7b6a07b33449c0c02e7f1a6994c40ba01d30e07
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-03 19:20:30 +0900

    Title: 문자에 따른 명령 2, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240203/문자에 따른 명령 2/text-based-commands2.py
+++ 240203/문자에 따른 명령 2/text-based-commands2.py
@@ -3,14 +3,16 @@ d = 1  #n1, e2, s3, w4
 
 move = input()
 for i in move:
+
+    if d > 4 or d < 1:
+            d = d % 4
+
     if i == 'L':
         d -= 1
     elif i == 'R':
         d += 1
     else:
-        if d > 4 or d <-4:
-            d = d % 4
-        elif d == 1:
+        if d == 1:
             loc[1] += 1
         elif d == 2:
             loc[0] += 1
