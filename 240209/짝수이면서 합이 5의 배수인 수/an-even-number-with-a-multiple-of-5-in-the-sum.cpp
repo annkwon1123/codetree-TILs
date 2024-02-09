96c9462f6f295bba1da2c116aae5033ba171479b
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-09 17:14:31 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240209/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
+++ 240209/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 string Complex(int n) {
     if(n % 2 != 0) return "No"; // false
-    int add = 0;
-    while(n == 0) {
-        add += n%10;
-        n = (n-n%10)/10;
-    }
-    if(add % 5 == 0) return "Yes";
+    if(((n/10) + (n%10)) % 5 == 0) return "Yes";
     else return "No"; 
 
 }
