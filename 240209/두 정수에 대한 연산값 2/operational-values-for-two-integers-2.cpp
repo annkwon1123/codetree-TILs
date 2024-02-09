e1bb67650d72d8d90da163f214b9f280df3c9166
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-09 21:05:17 +0900

    Title: 두 정수에 대한 연산값 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240209/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
+++ 240209/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void Caculation(int* a, int* b) {
+    if (*a > *b) {
+        *a = *a * 2;
+        *b = *b + 10;
+    } else {
+        *b = *b * 2;
+        *a = *a + 10;
+    }
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     cin >> a >> b;
-    if (a > b) {
-        cout << a*2 << " " << b+10 << endl;
-    } else {
-        cout << a+10 << " " << b*2 << endl;
-    }
+    Caculation(&a, &b);
+    cout << a << " " << b << endl;
     return 0;
 }
