f2c1d7c024a33237bbda68817aa6d4fe5f2071d7
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-09 16:09:57 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240209/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void PrintNLines(int n) {
+    for(int i = 0; i < n; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    PrintNLines(n);
+    return 0;
+}
