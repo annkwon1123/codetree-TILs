22bd1d8f72458654a5cde004a955e33d4a18c45f
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-10 12:40:55 +0900

    Title: 사칙연산 함수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/사칙연산 함수/quadratic-operations-function.cpp
+++ 240210/사칙연산 함수/quadratic-operations-function.cpp
@@ -8,15 +8,21 @@ int Ans(int a, char o, int b) {
         return a-b; 
     } else if(o == '/') {
         return a/b;
-    } else {
+    } else if(o == '*'){
         return a*b;
     }
+    return -999;
 }
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, c;
     char o;
     cin >> a >> o >> c;
-    cout << a << " " << o << " " << c << " = " << Ans(a, o, c) << endl; 
+
+    if(Ans(a, o, c) != -999){
+        cout << a << " " << o << " " << c << " = " << Ans(a, o, c) << endl; 
+    } else {
+        cout << "False" << endl;
+    }
     return 0;
 }
