da03f20512a459e702db71e6ae9c3023f7dba4c8
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-10 16:03:09 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int Fact(int n) {
+    if(n == 1) return 1;
+    return Fact(n - 1) + n;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    cout << Fact(n);
+    return 0;
+}
