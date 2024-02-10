1a680225cf8d739f93d0553580b0048534ec6557
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-10 20:38:17 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
+++ 240210/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -6,7 +6,7 @@ int main() {
     double a, b, c;
     cin >> a >> b >> c;
     cout << fixed;
-    cout.precision(2);
+    cout.precision(3);
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
