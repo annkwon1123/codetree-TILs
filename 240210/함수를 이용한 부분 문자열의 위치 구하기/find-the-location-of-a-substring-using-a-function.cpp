6b760a120376d981399c743c41869229acb6e7bd
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-10 14:36:18 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240210/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string str;
+
+int SubStr(string ss) {
+    int count = 1;
+    int len = str.length();
+    int subLen = ss.length();
+    for(int i = 0; i < len-subLen+1; i++) {
+        if(str[i] == ss[0]) {
+            for(int j = 1; j < subLen; j++) {
+                if(str[i+j] == ss[j]) count++; 
+            }
+            if(count == subLen) return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> str;
+    string subStr;
+    cin >> subStr;
+    cout << SubStr(subStr);
+    return 0;
+}
