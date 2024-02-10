393132ade043a8cfdf1a2ac822ce0c75cf1e9fab
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-10 22:39:43 +0900

    Title: 1이 3개 이상 있는 위치, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 240210/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int n;
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int x,y;
     int dx[4] = {0,1,0,-1}, dy[4] = {1,0,-1,0};
-    int n;
+    
     cin >> n;
 
     int arr[n][n];
@@ -20,7 +25,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             for(int k = 0; k < 4; k++) {
-                if(arr[i+dx[k]][j+dy[k]] == 1) good++;
+                if(InRange(i+dx[k],j+dy[k]) && arr[i+dx[k]][j+dy[k]] == 1) good++;
             }
             if(good > 2) count++;
             good = 0;
