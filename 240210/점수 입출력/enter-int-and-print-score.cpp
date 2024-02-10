78f7a88fba8ceedf37af5e6cd3e83347da257d6d
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-10 20:25:07 +0900

    Title: 점수 입출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/점수 입출력/enter-int-and-print-score.cpp
+++ 240210/점수 입출력/enter-int-and-print-score.cpp
@@ -4,6 +4,6 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     std::cin >> n;
-    std::cout << "Your score is " << n << " point."
+    std::cout << "Your score is " << n << " point.";
     return 0;
 }
