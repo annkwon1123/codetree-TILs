0328b8d25f99bf245e1f6602494597eee67a5195
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-11 11:28:55 +0900

    Title: 두 개의 동일한 수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240211/두 개의 동일한 수열/two-equal-series.cpp
+++ 240211/두 개의 동일한 수열/two-equal-series.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <algorithm>
 
+#define MAX_N 100
+
 using namespace std;
+
 int n;
-int A[100],B[100];
+int a[MAX_N], b[MAX_N];
+
+bool Equal() {
+    // n개의 원소를 순서대로 봤을 때
+    // 전부 동일한 경우에만 일치합니다.
+    // 단 하나라도 다르다면, false입니다.
+    for(int i = 0; i < n; i++)
+		if(a[i] != b[i])
+            return false;
+    
+    return true;
+}
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 입력
     cin >> n;
-    for(int i = 0; i < n; i++) cin >> A[n];
-    for(int i = 0; i < n; i++) cin >> B[n];
-    sort(A, A+n);
-    sort(B, B+n);
-    for(int i = 0; i < n; i++) {
-        if(A[i] != B[i]) {
-            cout << "No";
-            return 0;
-        }
-    }
-    cout << "Yes";
-    return 0;
+	
+	for(int i = 0; i < n; i++)
+	    cin >> a[i];
+	for(int i = 0; i < n; i++)
+		cin >> b[i];
+	
+    // 정렬
+	sort(a, a + n);
+	sort(b, b + n);
+    
+    // 수열이 일치하는지 확인합니다.
+    if(Equal())
+        printf("Yes");
+    else
+        printf("No");
+	return 0;
 }
