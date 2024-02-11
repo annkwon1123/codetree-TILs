0b66aeca60a626d2a3ce051af30e7dd4c5b73921
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-11 11:28:08 +0900

    Title: 두 개의 동일한 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240211/두 개의 동일한 수열/two-equal-series.cpp
+++ 240211/두 개의 동일한 수열/two-equal-series.cpp
@@ -2,13 +2,12 @@
 #include <algorithm>
 
 using namespace std;
+int n;
+int A[100],B[100];
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
     cin >> n;
-    int A[n] = {0,};
-    int B[n] = {0,};
     for(int i = 0; i < n; i++) cin >> A[n];
     for(int i = 0; i < n; i++) cin >> B[n];
     sort(A, A+n);
