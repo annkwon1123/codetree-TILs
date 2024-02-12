0bb686c9ab3388ab6f64a853fc7d4317750e9e10
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-12 22:05:56 +0900

    Title: 배열 채우고 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10];
+    for(int i = 0; i < 10; i++) cin >> arr[i];
+    for(int i = 0; i < 10; i++) cout << arr[10-i-1];
+    return 0;
+}
