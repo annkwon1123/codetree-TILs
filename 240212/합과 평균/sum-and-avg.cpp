4b42a677f1f7ac9fee32e145af1c37afd51a8218
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-12 14:13:14 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240212/합과 평균/sum-and-avg.cpp
+++ 240212/합과 평균/sum-and-avg.cpp
@@ -5,8 +5,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     double a, b;
     cin >> a >> b;
-    // cout << fixed;
-    // cout.precision(1);
-    cout << a+b << " " << (a+b)/2 << endl;
+    cout << a+b << " ";
+    cout << fixed;
+    cout.precision(1);
+    cout << (a+b)/2 << endl;
     return 0;
 }
