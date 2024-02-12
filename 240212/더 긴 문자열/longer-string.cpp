d68bc1e255bd4bb19288019cdc5e8c97c3014a53
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-12 14:44:06 +0900

    Title: 더 긴 문자열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240212/더 긴 문자열/longer-string.cpp
+++ 240212/더 긴 문자열/longer-string.cpp
@@ -7,7 +7,7 @@ int main() {
     string str1, str2;
     cin >> str1 >> str2;
     if(str1.length() > str2.length()) cout << str1 << " " << str1.length() << endl;
-    if(str1.length() == str2.length()) cout << "same" << endl;
+    else if(str1.length() == str2.length()) cout << "same" << endl;
     else cout << str2 << " " << str2.length() << endl;
     return 0;
 }
