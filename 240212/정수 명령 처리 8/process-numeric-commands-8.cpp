8fcd0f2b22e042a12ffdb3603253b36d9237c1af
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-12 23:59:24 +0900

    Title: 정수 명령 처리 8, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    list<int> s;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+        if(str == "push_back") {
+            int num; 
+            cin >> num;
+            s.push_back(num);
+        } else if(str == "push_front") {
+            int num; 
+            cin >> num;
+            s.push_front(num);
+        } else if(str == "pop_back") {
+            cout << s.back() << endl;
+            s.pop_back();
+        } else if(str == "pop_front") {
+            cout << s.front() << endl;
+            s.pop_front();
+        } else if(str == "back") {
+            cout << s.back() << endl;
+        } else if(str == "front") {
+            cout << s.front() << endl;
+        } else if(str == "size") {
+            cout << s.size() << endl;
+        } else {
+            if(s.empty()) cout << 1 << endl;
+            else cout << 0 << endl;
+        }
+    }
+    return 0;
+}
