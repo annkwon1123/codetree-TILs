97c2041b4a482269833693cfaddcdbeb64c6d4c1
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-12 22:28:18 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240212/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240212/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -4,22 +4,17 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int arr[10];
-    double tot1 = 0, tot2 = 0;
-    for(int i = 0; i < 9; i++) {
+    double tot = 0;
+    int i = 0;
+    for(i = 0; i < 9; i++) {
         cin >> arr[i];
-        if(arr[i] >= 250)tot1 += arr[i];
-        tot2 += arr[i];
+        if(arr[i] >= 250) break;
+        tot += arr[i];
     }
     
-    if(tot1 > 0) {
-        cout << tot1 << " "; 
-        cout << fixed;
-        cout.precision(2);
-        cout << tot1/10 << endl;
-    } else {
-        tot2 += arr[9];
-        cout << tot2 << " ";
-        cout << tot2/10 << endl;
-    }
+    cout << tot << " ";
+    cout << fixed;
+    cout.precision(1);
+    cout << tot/i;
     return 0;
 }
