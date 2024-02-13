d4b2a08a7960f69be5d7b5824e708fad303a0435
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-13 22:22:23 +0900

    Title: 황금비율 토스트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240213/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    list<char> l;
+    int n, m;
+    cin >> n >> m;
+    string str;
+    cin >> str;
+    for(int i = 0; i < n; i ++) {
+        l.push_back(str[i]);
+    }
+    
+    char c;
+    list<char>::iterator it;
+    it = l.end();
+    for(int i = 0; i < n; i++) {
+        cin >> c;
+        if(c == 'L') {
+            it--;
+        } else if(c == 'P') {
+            cin >> c;
+            l.insert(it, c);
+        } else if(c == 'R') {
+            it++;
+        } else {
+            l.erase(it);
+        }
+    }
+    
+    for(it = l.begin(); it != l.end(); it++) {
+        cout << *it;
+    }
+    return 0;
+}
