aa5420f9d8706fb8d7f866c0810261d6e8f3b080
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-18 21:35:44 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240218/요일 맞추기/guess-day-of-week.cpp
+++ 240218/요일 맞추기/guess-day-of-week.cpp
@@ -6,22 +6,17 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int m1,d1,m2,d2;
     cin >> m1 >> d1 >> m2 >> d2;
-    int month = m1, day = d1;
-    int arr[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    int arr1[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
     string arr2[7] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 
-    int take_days = 1;
-    do {
-        take_days ++;
-        day ++;
-        if(day > arr[month]) {
-            month++;
-            day = 1;
-        }
-    } while(month == m2 && day == d2);
+    int take_days1 = m1 * arr1[m1] + d1;
+    int take_days2 = m2 * arr1[m2] + d2;
+    int days = 0;
 
-    cout << arr2[take_days%7+1];
-
+    if(take_days1 < take_days2) days = take_days2 - take_days1;
+    else days = take_days1 - take_days2;
+    cout << arr2[days%7-1];
 
     return 0;
 }
