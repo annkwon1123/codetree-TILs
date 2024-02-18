c073e578962531fe2050bdcdc9256f0bd7bde5a0
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-18 20:31:09 +0900

    Title: Date to Date, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240218/Date to Date/date-to-date.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1,d1,m2,d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    int month = m1, day = d1;
+    int elapsed_days = 1;
+
+    int num_of_days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    while(true) {
+        if(month == m2 && day == d2) 
+            break;
+        elapsed_days++;
+        day++;
+
+        if(day > num_of_days[month]) {
+            month++;
+            day = 1;
+        }
+    }
+
+    cout << elapsed_days;
+    return 0;
+}
