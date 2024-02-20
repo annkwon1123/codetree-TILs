ccd86b9c9ea902bbbad45d33fc91164167f57d2d
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-20 20:40:59 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240220/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 240220/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define MAX_N 200
+#define OFFSET 100
 using namespace std;
 
 int arr[MAX_N][MAX_N];
@@ -12,6 +13,10 @@ int main() {
     for(int i = 0; i < n; i++) {
         int x1,y1,x2,y2;
         cin >> x1 >> y1 >> x2 >> y2;
+        x1 += OFFSET;
+        y1 += OFFSET;
+        x2 += OFFSET;
+        y2 += OFFSET;
         for(int j = x1; j < x2; j++) {
             for(int k = y1; k < y2; k++) {
                 arr[j][k] = 1;
