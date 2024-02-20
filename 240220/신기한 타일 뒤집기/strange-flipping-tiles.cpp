8712bfd92c44b2b71c6dfacaa092b2d5d9e4a887
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-20 21:57:00 +0900

    Title: 신기한 타일 뒤집기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240220/신기한 타일 뒤집기/strange-flipping-tiles.cpp
+++ 240220/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -5,8 +5,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    int max = -9999;
-    int min = 9999;
+    int max = 0;
+    int min = 0;
     int cur = 0;
 
     for(int i = 0; i < n; i++) {
