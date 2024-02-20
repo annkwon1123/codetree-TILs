498ca5b8e3e3ab4dfd78200981630925dd1a5e69
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-20 20:38:50 +0900

    Title: 사각형의 총 넓이 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240220/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#define MAX_N 200
+using namespace std;
+
+int arr[MAX_N][MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        int x1,y1,x2,y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for(int j = x1; j < x2; j++) {
+            for(int k = y1; k < y2; k++) {
+                arr[j][k] = 1;
+            }
+        }
+
+    }
+
+    int tot = 0;
+    for(int j = 0; j < MAX_N; j++) {
+        for(int k = 0; k < MAX_N; k++) {
+            if(arr[j][k] == 1) tot ++;
+        }
+    }
+    cout << tot;
+    return 0;
+}
