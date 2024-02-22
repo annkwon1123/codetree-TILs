4fac852e0e7331dd51f38ee0b2abd0e562d4f2c6
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-23 02:26:04 +0900

    Title: 좌우로 움직이는 로봇, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#define MAX_N 1000000
+using namespace std;
+
+int robotA[MAX_N];
+int robotB[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+
+    int time = 0, cur = 0;
+    int t;
+    char d;
+
+    for(int i = 0; i < n; i++) {
+        cin >> t >> d;
+        if(d == 'R') {
+            for(int j = 0; j < t; j++) {
+                robotA[j + time] = cur;
+                cur ++;
+            }
+            time += t;
+        }
+        else if(d == 'L') {
+            for(int j = 0; j < t; j++) {
+                robotA[j + time] = cur;
+                cur --;
+            }
+            time += t;
+        }
+    }
+
+    time = 0, cur = 0;
+    for(int i = 0; i < m; i++) {
+        cin >> t >> d;
+        if(d == 'R') {
+            for(int j = 0; j < t; j++) {
+                robotB[j + time] = cur;
+                cur ++;
+            }
+            time += t;
+        }
+        else if(d == 'L') {
+            for(int j = 0; j < t; j++) {
+                robotB[j + time] = cur;
+                cur --;
+            }
+            time += t;
+        }
+    }
+
+    int cnt = 0;
+    for(int i = 1; i < time; i++) {
+        if((robotA[i-1] != robotA[i]) && (robotB[i-1] != robotB[i]) && (robotA[i] == robotB[i])) cnt ++;
+    }
+    cout << cnt;
+    return 0;
+}
