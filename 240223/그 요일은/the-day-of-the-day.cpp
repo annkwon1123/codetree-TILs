512b5d6d06ca0508f2bb2994dd88dd7b900fc18e
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-23 02:09:20 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240223/그 요일은/the-day-of-the-day.cpp
+++ 240223/그 요일은/the-day-of-the-day.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int Days(int m, int d) {
     int tot = 0;
     int arr1[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
-    for(int i = 1; i < m; i++) tot += arr1[m];
+    for(int i = 1; i < m; i++) tot += arr1[i];
     tot += d;
     return tot;
 }
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int m1,d1,m2,d2;
@@ -19,6 +20,7 @@ int main() {
     int wd = 0; // week day
     string str; //cin buffer
     cin >> str; // 요일 저장
+
     for(int i = 0; i < 7; i ++) {
         if(str == arr2[i]) wd = i; // 요일 번호 저장
     }
@@ -27,7 +29,7 @@ int main() {
     // cout << Days(m2,d2) - Days(m1,d1) << endl;
 
     int cnt = diff/7; // 몇 주가 있는지
-    if((diff == 0 && wd == 0)|| wd <= diff%7) cnt ++;
+    if(diff == 0 && wd <= diff%7) cnt ++;
     // cout << diff/7 << " " << diff%7 << endl;
     cout << cnt;
     return 0;
