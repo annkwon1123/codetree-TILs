8140b33c8400dd8d0c7f76bcf94fc76305f3f4dc
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-23 00:53:15 +0900

    Title: 그 요일은, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int Days(int m, int d) {
+    int tot = 0;
+    int arr1[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    for(int i = 1; i < m; i++) tot += arr1[m];
+    tot += d;
+    return tot;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1,d1,m2,d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    string arr2[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    
+    int wd = 0; // week day
+    string str; //cin buffer
+    cin >> str; // 요일 저장
+    for(int i = 0; i < 7; i ++) {
+        if(str == arr2[i]) wd = i; // 요일 번호 저장
+    }
+    
+    int diff = Days(m2,d2) - Days(m1,d1);
+    // cout << Days(m2,d2) << " " << Days(m1,d1) << endl;
+    if(wd == 0) cout << (diff+wd)/7;
+    else cout << (diff+wd)/7 - 1;
+    return 0;
+}
