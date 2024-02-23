b06a629500b23e67b218513ae0084d5f5d0f4808
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-23 21:49:52 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, t;
+    cin >> n >> t;
+
+    int r,c;
+    char d;
+    cin >> r >> c >> d;
+
+    int dir_num;
+    if(d == 'U') dir_num = 0; // 아래
+    else if(d == 'D') dir_num = 1; // 위
+    else if(d == 'L') dir_num = 2; // 왼쪽
+    else dir_num = 3; // 오른쪽
+
+    int dx[4] = {0,0,-1,1}, dy[4] = {1,-1,0,0};
+
+    for(int i = 0; i < t; i++) {
+
+        r = r + dy[dir_num];
+        c = c + dx[dir_num];
+
+        if(c < 1) {
+            c = 1;
+            dir_num = 3; // 오른쪽으로 방향 바꾸기 
+            
+        }
+        else if(c > n) {
+            c = n;
+            dir_num = 2; // 왼쪽으로 방향 바꾸기
+        }
+        if(r < 1) {
+            r = 1;
+            dir_num = 1; // 위쪽으로 방향 바꾸기
+        }
+        else if(r > n) {
+            r = n;
+            dir_num = 0; // 아래쪽으로 방향 바꾸기
+        }
+        // cout << r << " " << c << " " << dir_num << endl;
+    }
+
+    cout << r << " " << c;
+    return 0;
+}
