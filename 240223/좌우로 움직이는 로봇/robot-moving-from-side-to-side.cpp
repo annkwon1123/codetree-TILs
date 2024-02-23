112fcb50bfbd75ac3c04a2988229e64f23ccbcf4
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-23 12:35:23 +0900

    Title: 좌우로 움직이는 로봇, Time: 46ms, Memory: 7MB, Status: Passed - CodeTree

--- 240223/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 240223/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -18,18 +18,17 @@ int main() {
         cin >> t >> d;
         if(d == 'R') {
             for(int j = 0; j < t; j++) {
-                robotA[j + time1] = cur;
                 cur ++;
+                robotA[j + time1] = cur;
             }
-            time1 += t;
         }
         else if(d == 'L') {
             for(int j = 0; j < t; j++) {
-                robotA[j + time1] = cur;
                 cur --;
+                robotA[j + time1] = cur;
             }
-            time1 += t;
         }
+        time1 += t;
     }
 
     int time2 = 0;
@@ -39,34 +38,38 @@ int main() {
         cin >> t >> d;
         if(d == 'R') {
             for(int j = 0; j < t; j++) {
-                robotB[j + time2] = cur;
                 cur ++;
+                robotB[j + time2] = cur;
             }
-            time2 += t;
         }
         else if(d == 'L') {
             for(int j = 0; j < t; j++) {
-                robotB[j + time2] = cur;
                 cur --;
+                robotB[j + time2] = cur;
             }
-            time2 += t;
         }
+        time2 += t;
     }
 
     // for(int i = 0; i < MAX_N; i++) cout << robotA[i] << " ";
     // for(int i = 0; i < MAX_N; i++) cout << robotB[i] << " ";
 
+    int locA, locB;
     int cnt = 0;
-    int time = time1 > time2 ? time2 : time1;
+    int time = time1 > time2 ? time1 : time2;
     
-    // cout << time << " " << time1 << " " << time2 << endl;
-
     for(int i = 1; i < time; i++) {
-        if(((robotA[i-1] != robotB[i-1])) && (robotA[i] == robotB[i])) cnt ++;
+        if(i < time1) locA = robotA[i];
+        if(i < time2) locB = robotB[i];
+        if((robotA[i-1] != robotB[i-1]) && (locA == locB)) cnt ++;
     }
+
     cout << cnt;
     return 0;
 }
+
+// -1 -2 -3 -2 -1 0 1 2 1 2
+// 1 2 3 4 3      2 1 0 1 2 3 4 3
 
 // -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13 -14 -15 -16 -17 -18
 // -1 -2 -3 -4 -5 -6 -7 -8 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 2 1 0 -1 -2 -3 -4
