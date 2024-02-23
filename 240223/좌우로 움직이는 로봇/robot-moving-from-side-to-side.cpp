75789de84db497292d8e672547fa715714bdba8a
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-23 10:43:24 +0900

    Title: 좌우로 움직이는 로봇, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240223/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 240223/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -10,7 +10,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int time = 0, cur = 0;
+    int time1 = 0, cur = 0;
     int t;
     char d;
 
@@ -18,46 +18,55 @@ int main() {
         cin >> t >> d;
         if(d == 'R') {
             for(int j = 0; j < t; j++) {
-                robotA[j + time] = cur;
+                robotA[j + time1] = cur;
                 cur ++;
             }
-            time += t;
+            time1 += t;
         }
         else if(d == 'L') {
             for(int j = 0; j < t; j++) {
-                robotA[j + time] = cur;
+                robotA[j + time1] = cur;
                 cur --;
             }
-            time += t;
+            time1 += t;
+        }
+    }
+
+    int time2 = 0;
+    cur = 0;
+
+    for(int i = 0; i < m; i++) {
+        cin >> t >> d;
+        if(d == 'R') {
+            for(int j = 0; j < t; j++) {
+                robotB[j + time2] = cur;
+                cur ++;
+            }
+            time2 += t;
+        }
+        else if(d == 'L') {
+            for(int j = 0; j < t; j++) {
+                robotB[j + time2] = cur;
+                cur --;
+            }
+            time2 += t;
         }
     }
 
     // for(int i = 0; i < MAX_N; i++) cout << robotA[i] << " ";
     // for(int i = 0; i < MAX_N; i++) cout << robotB[i] << " ";
 
-    time = 0, cur = 0;
-    for(int i = 0; i < m; i++) {
-        cin >> t >> d;
-        if(d == 'R') {
-            for(int j = 0; j < t; j++) {
-                robotB[j + time] = cur;
-                cur ++;
-            }
-            time += t;
-        }
-        else if(d == 'L') {
-            for(int j = 0; j < t; j++) {
-                robotB[j + time] = cur;
-                cur --;
-            }
-            time += t;
-        }
-    }
-
     int cnt = 0;
-    for(int i = 1; i < MAX_N; i++) {
+    int time = time1 > time2 ? time2 : time1;
+    
+    // cout << time << " " << time1 << " " << time2 << endl;
+
+    for(int i = 1; i < time; i++) {
         if(((robotA[i-1] != robotB[i-1])) && (robotA[i] == robotB[i])) cnt ++;
     }
     cout << cnt;
     return 0;
 }
+
+// -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13 -14 -15 -16 -17 -18
+// -1 -2 -3 -4 -5 -6 -7 -8 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 2 1 0 -1 -2 -3 -4
