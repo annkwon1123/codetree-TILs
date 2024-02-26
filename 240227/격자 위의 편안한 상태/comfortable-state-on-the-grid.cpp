c40ec2de370e891592004fae2c2883433050ec52
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-02-27 02:16:32 +0900

    Title: 격자 위의 편안한 상태, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

--- 240227/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 240227/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int n, m;
-    cin >> n >> m;
+int n, m;
 
-    int arr[n][n];
+bool IsRange(int x, int y) {
+    return ((x >= 0 && x < n) && (y >= 0 && y < n));
+}
+
+int main() {
+    
+    cin >> n >> m; // 입력 받기
+
+    int arr[n][n]; // 배열 선언 및 초기화
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             arr[i][j] = 0;
         }
     }
-    int r,c;
 
+    int r,c;
     int X,Y;
     int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
     int cnt = 0;
@@ -27,15 +32,23 @@ int main() {
         if(arr[X][Y] == 0) {
             arr[X][Y] = 1;
 
-            if(arr[X+dx[0]][Y+dy[0]] == 1) cnt ++;
-            if(arr[X+dx[1]][Y+dy[1]] == 1) cnt ++;
-            if(arr[X+dx[2]][Y+dy[2]] == 1) cnt ++;
-            if(arr[X+dx[3]][Y+dy[3]] == 1) cnt ++;
+            if(IsRange(X+dx[0], Y+dy[0]) && arr[X+dx[0]][Y+dy[0]] == 1) cnt ++;
+            if(IsRange(X+dx[1], Y+dy[1]) && arr[X+dx[1]][Y+dy[1]] == 1) cnt ++;
+            if(IsRange(X+dx[2], Y+dy[2]) && arr[X+dx[2]][Y+dy[2]] == 1) cnt ++;
+            if(IsRange(X+dx[3], Y+dy[3]) && arr[X+dx[3]][Y+dy[3]] == 1) cnt ++;
         }
-
+        
+        // cout << cnt << " ";
         if(cnt == 3) cout << 1 << endl;
         else cout << 0 << endl;
         cnt = 0;
+
+        // for (int i = 0; i < n; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         cout << arr[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
 
     }
     return 0;
