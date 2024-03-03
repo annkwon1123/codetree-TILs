f84d13ae9387132185f4c2b485efc13fea8b5364
Soyeon <86275994+annkwon1123@users.noreply.github.com>
2024-03-03 16:02:52 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240303/모이자/gather.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+int n;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    
+    int arr[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    int min_sum = 999;
+    for(int i = 0; i < n; i++) {
+
+        int sum_diff = 0;
+        for(int j = 0; j < n; j++) {
+            sum_diff += arr[j] * abs(j - i);
+        }
+
+        min_sum = min(min_sum, sum_diff);
+        sum_diff = 0;
+    }
+
+    cout << min_sum;
+    return 0;
+}
